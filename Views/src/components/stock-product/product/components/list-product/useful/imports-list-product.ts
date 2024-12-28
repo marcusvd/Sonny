7fbc0a39c95b7f47355234f39cfc054be6f29d0c
marcusvd <marcusmvd@gmail.com>
2024-12-27 21:14:36 -0300

    oneMore

--- Views/src/components/stock-product/product/components/list-product/useful/imports-list-product.ts
+++ Views/src/components/stock-product/product/components/list-product/useful/imports-list-product.ts
@@ -5,17 +5,13 @@ import { MatPaginatorModule } from "@angular/material/paginator"
 
 import { BtnGComponent } from "src/shared/components/btn-g/btn-g.component"
 
+import { MatIconModule } from "@angular/material/icon"
+import { ContainerCardModelAComponent } from "src/components/stock-product/product/components/list-product/container-card-model-a/container-card-model-a.component"
 import { BtnFilterGComponent } from "src/shared/components/btn-filter-g/btn-filter-g.component"
-import { ListCardGComponent } from "src/shared/components/list-g/list-card-g/list-card-g.component"
 import { ListGComponent } from "src/shared/components/list-g/list/list-g.component"
 import { SubTitleComponent } from "src/shared/components/sub-title/sub-title.component"
 import { TitleComponent } from "src/shared/components/title/components/title.component"
 import { FilterProductListComponent } from "../filter-search-product-list/filter-product-list.component"
-import { FooterCardModelAComponent } from "src/shared/components/card-g/card-g-model-a/footer-card-model-a/footer-card-model-a.component"
-import { HeaderCardModelAComponent } from "src/shared/components/card-g/card-g-model-a/header-card-model-a/header-card-model-a.component"
-import { ContentCardModelAComponent } from "src/shared/components/card-g/card-g-model-a/content-card-model-a/content-card-model-a.component"
-import { LineDividerCardModelAComponent } from "src/shared/components/card-g/card-g-model-a/card-line-divider-model-a-line/line-divider-card-model-a-line.component"
-import { MatIconModule } from "@angular/material/icon"
 
 export const ImportsListProduct:any[] =[
     CommonModule,
@@ -28,10 +24,11 @@ export const ImportsListProduct:any[] =[
     ListGComponent,
     FilterProductListComponent,
     BtnFilterGComponent,
-    ListCardGComponent,
-    HeaderCardModelAComponent,
-    ContentCardModelAComponent,
-    FooterCardModelAComponent,
-    LineDividerCardModelAComponent
+    ContainerCardModelAComponent
+    // ListCardGComponent,
+    // HeaderCardModelAComponent,
+    // ContentCardModelAComponent,
+    // FooterCardModelAComponent,
+    // LineDividerCardModelAComponent
 
 ]
