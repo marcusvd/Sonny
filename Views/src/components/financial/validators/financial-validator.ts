48718d890e773cab5a222e26ea6bd62a86e68d0d
marcusvd <marcusmvd@gmail.com>
2025-06-26 18:24:20 -0300

    oneMore

--- Views/src/components/financial/validators/financial-validator.ts
+++ Views/src/components/financial/validators/financial-validator.ts
@@ -3,50 +3,43 @@
 
 import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
 
+// export function CreditLimitValidator(): ValidatorFn {
+
+//   return (control: AbstractControl): ValidationErrors | null => {
+
+//     const group = control as any;
+
+//     const typeIsDebit = (+group.get('type').value > 0);
+//     const creditLimit = !(+group.get('creditLimit').value > 100);
+
+//     if (typeIsDebit && creditLimit) {
+//       group.get('creditLimit').setValidators(Validators.required);
+//     }
+//     else {
+//       group.get('creditLimit').setErrors(null);
+//     }
+
+//     console.log(typeIsDebit && creditLimit)
+
+//     return null;
+
+//   }
+// }
+
+
+
 export function CreditLimitValidator(): ValidatorFn {
+  return (group: AbstractControl): ValidationErrors | null => {
+    const type = group.get('type')?.value;
+    const creditLimit = group.get('creditLimit')?.value;
 
-  return (control: AbstractControl): ValidationErrors | null => {
-
-    const group = control as any;
-
-    const typeIsDebit = (+group.get('type').value > 0);
-    const creditLimit = !(+group.get('creditLimit').value > 100);
-
-    if (typeIsDebit && creditLimit) {
-      group.get('creditLimit').setValidators(Validators.required);
+    if (type > 0 && (creditLimit === null || creditLimit === undefined || creditLimit === 0)) {
+      return { creditLimitRequired: true };
     }
-    else {
-      group.get('creditLimit').setErrors(null);
-    }
-
-    console.log(typeIsDebit && creditLimit)
-    // console.log(+group.get('creditLimit').value <= 0)
-    // console.log(+group.get('creditLimit').errors)
-    // console.log(group.get('creditLimit').errors)
-    // const partnerId = group.get('partnerId').value as number;
-    // const partnerId = group.get('partnerId').value as number;
-    // const base = group.get('base').value as boolean;
-
-    // const isValid = (customerId || partnerId || base)
-
-    // if (!isValid) {
-    //   isDebit
-    //   group.get('partnerId').setErrors({ atleastone: true });
-    //   group.get('base').setErrors({ atleastone: true });
-    // }
-    // else {
-    //   group.get('customerId').setErrors(null);
-    //   group.get('partnerId').setErrors(null);
-    //   group.get('base').setErrors(null);
-    // }
-
 
     return null;
-
-  }
+  };
 }
-
-
 
 export class FinancialValidator {
 
